3f74d4f3fef5beed0ef4fdcd1345ddd9442659b2
MacRos1 <>
2021-05-06 13:50:45 +0000

    updating

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-alert('Welcome To Your Home Of Movies');
 let age = prompt("who old are you?")
-
-if(age >= 16){
-    alert('You are allowed to stay');
-}else{
-    alert("You are not allowed to stay. Get out now!!");
+while(age < 16){
+    age = prompt('You entered an invalid input your age must be 16 or greater.');
 }
+
+alert('Welcome To Your Home Of Movies');
 
 // Assigning movies description from the internet
 let theExpendables2 = `The Expendables 2 is a 2012 American action film directed by Simon West, written by Richard Wenk and Sylvester Stallone and based on a story by Ken Kaufman, David Agosto and Wenk. Brian Tyler returned to score the film. It is the sequel to the 2010 action film The Expendables, and stars Sylvester Stallone, Jason Statham, Jet Li, Dolph Lundgren, Chuck Norris, Terry Crews, Randy Couture, Liam Hemsworth, Jean-Claude Van Damme, Bruce Willis, and Arnold Schwarzenegger. The story follows the mercenary group known as "the Expendables" as they undertake a seemingly simple mission which evolves into a quest for revenge against rival mercenary Jean Vilain, who murdered one of their own and threatens the world with a deadly weapon. It is the second installment in The Expendables trilogy.`
@@ -23,31 +21,46 @@ let Nobody = `Nobody is a 2021 American action thriller film directed by Ilya Na
 let Braveheart = `Braveheart is a 1995 American epic historical fiction war film directed and co-produced by Mel Gibson, who portrays William Wallace, a late-13th-century Scottish warrior. The film depicts the life of Wallace leading the Scots in the First War of Scottish Independence against King Edward I of England. The film also stars Sophie Marceau, Patrick McGoohan and Catherine McCormack. The story is inspired by Blind Harry's 15th century epic poem The Actes and Deidis of the Illustre and Vallyeant Campioun Schir William Wallace and was adapted for the screen by Randall Wallace.`
 
 
-
 //  Geting element from the DOM
 let movCard = document.querySelectorAll('.mov-card');
 let movInfo = document.querySelector('.mov-info');
 let movInfoContainer = document.createElement('div');
 let movInfoImage = document.createElement('img');
 let movInfoText = document.createElement('p');
+let starsContainer = document.createElement('div');
 
 // Appending element inside of the parent element container
 movInfoContainer.appendChild(movInfoImage);
 movInfoContainer.appendChild(movInfoText);
 movInfo.appendChild(movInfoContainer);
+movInfo.appendChild(starsContainer);
 
 
+// adding eventListener for each moive card
 movCard.forEach(card =>{
     card.addEventListener('click', function(e){
 
         // Geting the movie title
         let movTitle = e.target.nextElementSibling;
+
         // Attaching styling classes to elements
         movInfo.classList.add('displayed');
         movInfoContainer.classList.add('movInfocontainer');
-        movInfoContainer.classList.add('movInfocontainer');
         movInfoImage.classList.add('mov-info__img');
         movInfoText.classList.add('mov-info__text');
+        starsContainer.classList.add('stars-container');
+        
+        // prompt to the user how many star he/she rate the movie
+        let movRateStars = prompt('How do you rate the movie by stars? (1 -5)');
+        movRateStars = movRateStars > 5 ? 5 : movRateStars;
+
+        for(let i = 0; i < movRateStars; i++){
+            // create stars imgs
+            let rateStar = document.createElement('img');
+            rateStar.classList.add('rate-star');
+            rateStar.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Star_icon_stylized.svg/512px-Star_icon_stylized.svg.png');
+            starsContainer.appendChild(rateStar);
+        }
 
         // Seting the src attrebute to the new img tag
         movInfoImage.setAttribute('src', e.target.getAttribute('src'));
@@ -71,12 +84,18 @@ movCard.forEach(card =>{
             movInfoText.textContent = '';
             movInfoText.appendChild(document.createTextNode(Braveheart));
         }
+
     });
+
 })
+
 
 // Closing the movie info popup handler
 document.addEventListener('click', function(e){
     if(movInfo.contains(e.target) && e.target.tagName ==='DIV'){
+        // close the popup window
         movInfo.classList.remove('displayed');
+        // remove all stars
+        starsContainer.innerHTML = '';
     }
 });
